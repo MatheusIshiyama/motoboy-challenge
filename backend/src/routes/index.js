a02433f7f4a297889c11a5b428420f9b04b006c7
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-06-19 17:17:09 -0300

    feat: auth middleware

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 
+const { authMiddleware } = require("../middlewares");
+
 const statusCheckRoute = require("./statusCheck");
 const usersRoute = require("./users");
 const signUpRouter = require("./signUp");
@@ -7,5 +9,7 @@ const signUpRouter = require("./signUp");
 router.use("/status", statusCheckRoute);
 router.use("/users", usersRoute);
 router.use("/signup", signUpRouter);
+
+router.use(authMiddleware);
 
 module.exports = router;
