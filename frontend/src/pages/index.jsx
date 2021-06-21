3e5c793ca8f8d449bd6dcbc612b3cf4da383c72f
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-06-21 18:54:40 -0300

    feat: verify if has token

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(async () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) await router.push("/login");
+  }, []);
+
   return <div>Home</div>;
 }
