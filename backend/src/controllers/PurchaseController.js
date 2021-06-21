afeda0cb5efa2f1f48eb5dc4ca5bb7fe773f3381
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-06-21 18:10:52 -0300

    feat: create purchase

--- backend/src/controllers/PurchaseController.js
+++ backend/src/controllers/PurchaseController.js
@@ -1,4 +1,4 @@
-const { PurchaseModel } = require("../models");
+const { PurchaseModel, ProductModel } = require("../models");
 const { Token } = require("../utils");
 
 const { validateToken } = Token;
@@ -14,6 +14,39 @@ class PurchaseController {
     const purchases = await PurchaseModel.find({ userId: user.id });
 
     res.status(200).json({ message: "Purchases founded", data: purchases });
+  }
+
+  async create(req, res) {
+    const { authorization } = req.headers;
+    let { productName, priceUnit, units, latitude, longitude } = req.body;
+
+    const [, token] = authorization.split(" ");
+
+    const user = await validateToken(token);
+
+    if (!productName || !priceUnit || !units || !latitude || !longitude)
+      return res.status(400).json({ message: "Missing fields on body" });
+
+    productName = productName.toLowerCase();
+
+    const productExists = await ProductModel.findOne({ name: productName });
+
+    if (!productExists) return res.status(404).json({ message: "Product not founded" });
+
+    const newPurchase = new PurchaseModel({
+      productId: productExists._id,
+      userId: user.id,
+      purchasedAt: Date.now(),
+      priceUnit,
+      units,
+      total: priceUnit * units,
+      latitude,
+      longitude,
+    });
+
+    await newPurchase.save();
+
+    res.status(200).json({ message: "Purchase registered", data: newPurchase });
   }
 }
 
