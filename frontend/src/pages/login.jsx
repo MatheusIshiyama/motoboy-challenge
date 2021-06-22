685d6a2f9b74ec64d4f19400a3c6423c72b00a1d
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-06-22 17:07:43 -0300

    feat: login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import styles from "../styles/login.module.scss";
+
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+  return (
+    <div className={styles.container}>
+      <form className={styles.card}>
+        <h1>Login</h1>
+        <div>
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" onClick={(e) => handleSubmit(e)}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
